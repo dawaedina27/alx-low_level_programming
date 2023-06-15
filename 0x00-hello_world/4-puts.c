8e7f76a40723aa6cd829d2ab5c1da60a202e15ac
dawaedina27 <dawaedina27@gmail.com>
2023-06-15 15:52:35 -0700

    Updated the 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<studio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	puts("\"programming is Programming is like building a multilingual puzzle");
