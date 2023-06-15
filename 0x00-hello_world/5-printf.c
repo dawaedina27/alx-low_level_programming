ebea5433088475533627d7ffc97f522870046d16
dawaedina27 <dawaedina27@gmail.com>
2023-06-15 15:19:50 -0700

    This is a general commit for the 0x00. C - Hello, World project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/** main - Entry point
+ *
+ * Return: Always 0 9success)
+ */
+
+int main(void)
+{
+	rintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
