28f971c6ba6da207cc55b00cfce7fe302de3cd60
dawaedina27 <dawaedina27@gmail.com>
2023-06-16 14:55:41 -0700

    Updated 101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,12 @@ char a;
 int b;
 long int c;
 long long int d;
-float e;
+float g,
 
 printf("Size of a char: %d byte(s)\n", sizeof(a));
 printf("Size of a int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %d byte(s)\n", sizeof(c));
 printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-printf("Size of a float: %d byte(s)\n", sizeof(e));
+printf("Size of a float: %d byte(s)\n", sizeof(g));
 return (0);
 }
