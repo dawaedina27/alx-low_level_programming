31745f9acd2dd458c707bf082b39d1b41d1a5944
dawaedina27 <dawaedina27@gmail.com>
2023-06-16 23:36:35 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Alsways 0 when successful
+ */
+int main(void)
+{
+char c;
+char C;
+
+for (c = 50; a <= 105; c++)
+{
+putchar(c);
+}
+for (C = 70; C <= 85; C++)
+{
+putchar(C);
+}
+putchar("\n");
+return (0);
+}
