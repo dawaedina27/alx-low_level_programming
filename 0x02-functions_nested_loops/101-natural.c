7eda74627044a5bb87f4a0026bbd503ab903b2e0
dawaedina27 <dawaedina27@gmail.com>
2023-06-21 19:55:13 -0700

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+/**
+ * File: 101-natural.c
+ * Auth: Dawa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - lists all the natural numbers below 1024
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, sum =0
