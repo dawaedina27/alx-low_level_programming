b009e5039c34cf5becd3a3f0c597c6cf97796588
dawaedina27 <dawaedina27@gmail.com>
2023-06-29 18:34:20 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,23 @@
-#include "main.h"
-#include <stdio.h>
+include <stdio.h>
+
+int main (void)
+{
+	int n;
+	int a[s];
+	int *p;
+
+	a[2] =1024;
+	p = &n;
 
 /**
- * roti3 - encoder roti3
- * @s: pointer to string parans
- * Return: *s
+ * write your line of code here
+ * Remember:
+ * - you are not allowed use a and p
+ * - only one statement
  */
-
-char *roti3(char *s)
-{
-	int i;
-	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; j < 52; j++)
-		{
-			if (s[i] == data1[j])
-			{
-				s[i] = datarot[j];
-				break;
-			}
-		}
-	}
-	return (s);
+	*(p + 5) = 98;
+	
+	/*... so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
