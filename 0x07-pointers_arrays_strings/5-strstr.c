cf09223e597bf82a97e0866781c7d426898fa66b
dawaedina27 <dawaedina27@gmail.com>
2023-07-03 17:22:04 -0700

    My primary goal of hacking was the intellectual curiosity, the seduction of adventure

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack, != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *p = needle
+			while (! == 'p && *p != '\0')
+			{
+				i++;
+				p++;
+			}
+		if (*p == '\0')
+			return (0);
+	}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (success)
+ */
+
+void print_chessboard(char (*2)[a])
+{
+	int i;
+	int j;
+	
+	for (i = 0; i < 8; i++)
+	{
+		for (i = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\0');
+	}
+}
