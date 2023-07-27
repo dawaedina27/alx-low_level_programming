9996285be1d5ed0462df9e0d8ee65e3f5dcd1877
dawaedina27 <dawaedina27@gmail.com>
2023-07-27 15:01:26 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,29 @@
-/**
- * File: 0-print_list.c
- * Author: Dawa Edina Hillary
- */
-
-
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a linked
  * @h: pointer to the list_t list to print
- *
+ * Auth: Dawa
  * Return: the number of printed nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	int s = 0;
+	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 		{
 			printf("[0] (nil)\n");
-			h = h->next;
 		}
+
 		else
-		{
+
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
+		h = h->next;
+
 		s++;
 	}
 
