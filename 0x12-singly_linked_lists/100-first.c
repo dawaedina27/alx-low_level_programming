a04539e302f4024f42e4cff00347c03c276e8560
dawaedina27 <dawaedina27@gmail.com>
2023-07-27 11:32:34 -0700

    Header files, Task 0 to Task 6

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * File: 100-first.c
+ * Author: Dawa Edina Hillary
+ */
+
+void __attribute__((constructor)) first(void);
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
